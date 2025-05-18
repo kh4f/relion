2b64616b6e55b56a392a86387d2b5db8d47d0f47
Kh4f <khafg050@gmail.com>
2025-05-18 18:28:13 +0300

    refactor: move main project files into 'src' directory

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import ryly from '../index.js';
-import cmdParser from '../command.js';
+import ryly from '../src/index.js';
+import cmdParser from '../src/commands.js';
 
 ryly(cmdParser.argv).catch((err) => {
 	console.error(err);
