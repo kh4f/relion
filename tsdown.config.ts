f7f81b117ca9de8c5d7cad1879049a802fbb1cce
Kh4f <khafg050@gmail.com>
2025-06-24 11:08:00 +0300

    build(tsdown): setup `tsdown`

---
+++ tsdown.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'tsdown'
+
+const isProd = process.argv.includes('--production')
+
+const common = defineConfig({
+	outDir: './dist',
+	sourcemap: isProd ? false : 'inline',
+	minify: isProd,
+})
+
+export default defineConfig([
+	{
+		...common,
+		entry: {
+			index: './src/index.ts',
+			preset: './src/preset/index.ts',
+		},
+		dts: true,
+		loader: {
+			'.hbs': 'text',
+		},
+	},
+	{
+		...common,
+		entry: './src/cli.ts',
+		dts: false,
+		outputOptions: {
+			banner: `#!/usr/bin/env node\n`,
+		},
+	},
+])
