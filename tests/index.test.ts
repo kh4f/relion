9bcc6b36daed32e6af3d1b32e808219a9a783446
kh4f <kh4f.dev@gmail.com>
2025-09-06 10:04:06 +0300

    test(index): add lifecycle test
    Adds a test to verify bump, changelog, commit, and tag lifecycles with dry run.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,27 @@
 import { describe, it, expect } from 'vitest'
+import relion from '@/index'
 
 describe('Smoke test', () => {
 	it('should pass', () => {
 		expect(1 + 1).toBe(2)
+	})
+})
+
+describe('Test all lifecycles', () => {
+	it('should bump version, generate changelog, commit and tag changes', async () => {
+		await relion({
+			versionSourceFile: 'package.json',
+			bump: [],
+			changelog: {
+				outputFile: 'temp/CHANGELOG.md',
+			},
+			commit: {
+				gpgSign: true,
+			},
+			tag: {
+				gpgSign: true,
+			},
+			dryRun: true,
+		})
 	})
 })
