3cdc734f1867e2edc815d0410e62cf4e6bdd1b2c
kh4f <kh4f.dev@gmail.com>
2025-09-08 12:08:36 +0300

    refactor(cli): move CLI logic in `runCli` exported function

--- src/cli.ts
+++ src/cli.ts
@@ -4,71 +4,75 @@ import relion from './index.js'
 import type { UserConfig } from '@/types'
 import { cli } from 'cleye'
 
-let config: UserConfig
+if (import.meta.main) await runCli()
 
-try {
-	const configFileURL = pathToFileURL(resolve(process.cwd(), 'relion.config.ts')).href
-	config = ((await import(configFileURL)) as { default: UserConfig }).default
-} catch (error) {
-	throw new Error(`Error loading config: ${(error as Error).message}`)
+export async function runCli(config?: UserConfig, argvs?: string[]) {
+	if (!config) {
+		try {
+			const configFileURL = pathToFileURL(resolve(process.cwd(), 'relion.config.ts')).href
+			config = ((await import(configFileURL)) as { default: UserConfig }).default
+		} catch (error) {
+			throw new Error(`Error loading config: ${(error as Error).message}`)
+		}
+	}
+
+	const argv = cli({
+		name: 'relion',
+		flags: {
+			bump: {
+				alias: 'b',
+				type: Boolean,
+				description: 'Bump the version',
+				default: false,
+			},
+			changelog: {
+				alias: 'l',
+				type: Boolean,
+				description: 'Generate a changelog',
+				default: false,
+			},
+			commit: {
+				alias: 'c',
+				type: Boolean,
+				description: 'Create a commit',
+				default: false,
+			},
+			tag: {
+				alias: 't',
+				type: Boolean,
+				description: 'Create a tag',
+				default: false,
+			},
+			profile: {
+				alias: 'p',
+				type: String,
+				description: 'Use config profile',
+			},
+			dryRun: {
+				alias: 'd',
+				type: Boolean,
+				description: 'Run without making any changes',
+				default: false,
+			},
+			latest: {
+				alias: 'L',
+				type: Boolean,
+				description: 'Use the latest-release commit range in changelog',
+				default: false,
+			},
+		},
+	}, undefined, argvs)
+
+	if (!argv.flags.bump) config.bump = false
+	if (!argv.flags.changelog) config.changelog = false
+	if (!argv.flags.commit) config.commit = false
+	if (!argv.flags.tag) config.tag = false
+	if (argv.flags.profile) config.profile = argv.flags.profile
+	if (argv.flags.dryRun) config.dryRun = argv.flags.dryRun
+	if (argv.flags.latest && config.changelog) {
+		if (config.changelog === true) config.changelog = {}
+		config.changelog.commitRange = 'latest-release'
+	}
+
+	await relion(config)
 }
-
-const argv = cli({
-	name: 'relion',
-	flags: {
-		bump: {
-			alias: 'b',
-			type: Boolean,
-			description: 'Bump the version',
-			default: false,
-		},
-		changelog: {
-			alias: 'l',
-			type: Boolean,
-			description: 'Generate a changelog',
-			default: false,
-		},
-		commit: {
-			alias: 'c',
-			type: Boolean,
-			description: 'Create a commit',
-			default: false,
-		},
-		tag: {
-			alias: 't',
-			type: Boolean,
-			description: 'Create a tag',
-			default: false,
-		},
-		profile: {
-			alias: 'p',
-			type: String,
-			description: 'Use config profile',
-		},
-		dryRun: {
-			alias: 'd',
-			type: Boolean,
-			description: 'Run without making any changes',
-			default: false,
-		},
-		latest: {
-			alias: 'L',
-			type: Boolean,
-			description: 'Use the latest-release commit range in changelog',
-			default: false,
-		},
-	},
-})
-
-if (!argv.flags.bump) config.bump = false
-if (!argv.flags.changelog) config.changelog = false
-if (!argv.flags.commit) config.commit = false
-if (!argv.flags.tag) config.tag = false
-if (argv.flags.profile) config.profile = argv.flags.profile
-if (argv.flags.dryRun) config.dryRun = argv.flags.dryRun
-if (argv.flags.latest && config.changelog) {
-	if (config.changelog === true) config.changelog = {}
-	config.changelog.commitRange = 'latest-release'
-}
-
-await relion(config)
