da665c4aa891aea872e0a8602ccf1dd9d97c3a7f
kh4f <kh4f.dev@gmail.com>
2025-09-09 09:12:58 +0300

    refactor(utils): move `deepLog` function to `logger.ts` module

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,9 +3,3 @@ export * from './version-manager'
 export * from './commits-parser'
 export * from './git-utils'
 export * from './logger'
-
-import { inspect } from 'node:util'
-
-export const deepLog = (value: unknown, depth?: number): void => {
-	console.log(inspect(value, { depth: depth ?? null, colors: true }))
-}
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,13 @@
+import { inspect } from 'node:util'
+
 let silent = false
 
 export const setSilent = (value: boolean) => silent = value
 
 export const log = (...args: unknown[]) => {
 	if (!silent) console.log(...args)
+}
+
+export const deepLog = (value: unknown, depth?: number): void => {
+	log(inspect(value, { depth: depth ?? null, colors: true }))
 }
