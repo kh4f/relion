0afb9de8fab8a4be291a64228c05efd314aaa453
kh4f <kh4f.dev@gmail.com>
2025-09-14 12:09:32 +0300

    refactor(templates): remove unused `index.ts` module

--- src/templates/index.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'node:fs'
-import { join } from 'node:path'
-
-const readTemplateFile = (name: string) => readFileSync(join(import.meta.dirname, `${name}.hbs`), 'utf8')
-
-export const releaseTemplate = readTemplateFile('release')
