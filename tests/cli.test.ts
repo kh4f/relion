b4b5e5cbbe05a0701c4a633be765556eac30037c
kh4f <kh4f.dev@gmail.com>
2025-09-20 18:31:55 +0300

    test(cli): pass config directly to `runCli` as a parameter

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -4,15 +4,25 @@ import { runCli } from '@/cli'
 describe('CLI', () => {
 	it('should print help message when `--help` is passed', async () => {
 		const exitSpy = vi.spyOn(process, 'exit').mockImplementation(() => undefined as never)
-		await runCli(undefined, ['--help'])
+		await runCli({}, ['--help'])
 		expect(exitSpy).toHaveBeenCalledWith(0)
 	})
 
 	it('should simulate all lifecycles with options from config file', async () => {
-		await runCli(undefined, ['-blmt', '-d'])
+		await runCli({}, ['-blmt', '-d'])
 	})
 
 	it(`should output latest release changelog using 'github' profile`, async () => {
-		await runCli(undefined, ['-l', '-p', 'github', '-L'])
+		await runCli({
+			_github: {
+				silent: true,
+				context: { commitHyperlink: false },
+				changelog: {
+					output: 'stdout',
+					header: '',
+					partials: { header: '' },
+				},
+			},
+		}, ['-l', '-p', 'github', '-L'])
 	})
 })
