3b7b5b6f46267205a3c740ced1e36ef675ed9bed
kh4f <kh4f.dev@gmail.com>
2025-09-20 16:16:46 +0300

    test(cli): remove timeouts

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -8,11 +8,11 @@ describe('CLI', () => {
 		expect(exitSpy).toHaveBeenCalledWith(0)
 	})
 
-	it('should simulate all lifecycles with options from config file', { timeout: 10000 }, async () => {
+	it('should simulate all lifecycles with options from config file', async () => {
 		await runCli(undefined, ['-blct', '-d'])
 	})
 
-	it(`should output latest release changelog using 'github' profile`, { timeout: 10000 }, async () => {
+	it(`should output latest release changelog using 'github' profile`, async () => {
 		await runCli(undefined, ['-l', '-p', 'github', '-L'])
 	})
 })
