7bb39588e2a95ede1dbba5c5801315a8935a961d
kh4f <kh4f.dev@gmail.com>
2025-09-20 16:15:36 +0300

    test(cli): make test names clearer and consistent

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,18 +1,18 @@
 import { describe, it, expect, vi } from 'vitest'
 import { runCli } from '@/cli'
 
-describe('Test cli', () => {
-	it('should print help message', async () => {
+describe('CLI', () => {
+	it('should print help message when `--help` is passed', async () => {
 		const exitSpy = vi.spyOn(process, 'exit').mockImplementation(() => undefined as never)
 		await runCli(undefined, ['--help'])
 		expect(exitSpy).toHaveBeenCalledWith(0)
 	})
 
-	it('should run all lifecycles with options from config file in dry run mode', { timeout: 10000 }, async () => {
+	it('should simulate all lifecycles with options from config file', { timeout: 10000 }, async () => {
 		await runCli(undefined, ['-blct', '-d'])
 	})
 
-	it(`should generate changelog with 'github' profile`, { timeout: 10000 }, async () => {
+	it(`should output latest release changelog using 'github' profile`, { timeout: 10000 }, async () => {
 		await runCli(undefined, ['-l', '-p', 'github', '-L'])
 	})
 })
