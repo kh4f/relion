eaf1747fb2cd374b7dbd529e233a0f28745bfee2
kh4f <kh4f.dev@gmail.com>
2025-09-21 11:33:54 +0300

    refactor(release): explicitly set `bump` and `changelog` options in config instead of passing flags in release scripts

--- relion.config.ts
+++ relion.config.ts
@@ -1,26 +1,20 @@
 import { defineConfig } from '@/.'
 
 export default defineConfig({
-	commit: {
-		gpgSign: true,
-	},
-	tag: {
-		gpgSign: true,
-	},
+	bump: true,
+	changelog: true,
+	commit: { gpgSign: true },
+	tag: { gpgSign: true },
 	_github: {
 		bump: false,
 		commit: false,
 		tag: false,
 		silent: true,
-		context: {
-			commitHyperlink: false,
-		},
+		context: { commitHyperlink: false },
 		changelog: {
 			output: 'stdout',
 			header: '',
-			partials: {
-				header: '',
-			},
+			partials: { header: '' },
 		},
 	},
 })
