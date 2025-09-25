9565cbdc3c4ec46ceac4a74f4f01fffb0664bdde
kh4f <kh4f.dev@gmail.com>
2025-09-25 11:10:30 +0300

    types(result): update `RelionResult` to use `null` instead of `undefined` for optional properties
    Explicitly set `null` for `generatedChangelog`, `commitCommand`, `tagCommand`, and `bumpResults` in `RelionResult` type.

--- src/types/result.d.ts
+++ src/types/result.d.ts
@@ -3,9 +3,9 @@ import type { ResolvedConfig, VersionedFile } from '@/types'
 export interface RelionResult {
 	resolvedConfig: ResolvedConfig
 	generatedChangelog: string | null
-	commitCommand?: string
-	tagCommand?: string
-	bumpResults?: BumpResult[]
+	commitCommand: string | null
+	tagCommand: string | null
+	bumpResults: BumpResult[] | null
 }
 
 export interface BumpResult extends VersionedFile {
