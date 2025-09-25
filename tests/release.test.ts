77fff5da330f70be81d6acc1b7783c181051b0eb
kh4f <kh4f.dev@gmail.com>
2025-09-25 07:30:51 +0300

    refactor(tests): replace `testConfig` with `silent: true` in tag generation tests

--- tests/release.test.ts
+++ tests/release.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from 'vitest'
 import relion from '@/.'
-import { testConfig } from './fixtures/relion.test-config'
 
 describe('release workflow', () => {
 	it('should simulate full release workflow', () => {
@@ -16,23 +15,36 @@ describe('release workflow', () => {
 
 describe('tag generation', () => {
 	it('should use default newTagFormat value', () => {
-		expect(relion({ ...testConfig }).resolvedConfig.context.newTag).toMatch(/^v\d+\.\d+\.\d+/)
+		expect(relion({ silent: true }).resolvedConfig.context.newTag).toMatch(/^v\d+\.\d+\.\d+/)
 	})
 
 	it('should use custom newTagFormat value', () => {
-		expect(relion({ ...testConfig,
+		expect(relion({ silent: true,
 			newTagFormat: 'release-{{version}}-beta',
 		}).resolvedConfig.context.newTag).toMatch(/^release-\d+\.\d+\.\d+-beta/)
 	})
 
 	it('should use custom newTagPrefix value', () => {
-		expect(relion({ ...testConfig,
+		expect(relion({ silent: true,
 			newTagPrefix: 'relion@',
 		}).resolvedConfig.context.newTag).toMatch(/^relion@\d+\.\d+\.\d+/)
 	})
 
 	it('should prefer newTagPrefix over newTagFormat', () => {
-		expect(relion({ ...testConfig,
+		expect(relion({ silent: true,
+			newTagPrefix: 'relion@',
+			newTagFormat: 'release-{{version}}-beta',
+		}).resolvedConfig.context.newTag).toMatch(/^relion@\d+\.\d+\.\d+/)
+	})
+
+	it('should use custom newTagPrefix value', () => {
+		expect(relion({ silent: true,
+			newTagPrefix: 'relion@',
+		}).resolvedConfig.context.newTag).toMatch(/^relion@\d+\.\d+\.\d+/)
+	})
+
+	it('should prefer newTagPrefix over newTagFormat', () => {
+		expect(relion({ silent: true,
 			newTagPrefix: 'relion@',
 			newTagFormat: 'release-{{version}}-beta',
 		}).resolvedConfig.context.newTag).toMatch(/^relion@\d+\.\d+\.\d+/)
