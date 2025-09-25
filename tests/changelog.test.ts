daba3b565858471ef12171b52d8e42694f6270e3
kh4f <kh4f.dev@gmail.com>
2025-09-25 07:25:28 +0300

    test(changelog): separate manual changelog inspection from automated tests

--- tests/changelog.test.ts
+++ tests/changelog.test.ts
@@ -3,32 +3,21 @@ import relion from '@/.'
 import { testConfig } from './fixtures/relion.test-config'
 
 describe('changelog generation', () => {
+	it.for(['v0.7.0', 'v0.8.0'])('should print changelog for release $0', (releaseTag) => {
+		expect(relion({ ...testConfig, changelog: { commitRange: { releaseTag } } }).generatedChangelog).toMatchSnapshot()
+	})
+})
+
+describe.runIf(process.env.VITEST_VSCODE)('manual changelog inspection', () => {
 	it('should print upcoming release changelog', () => {
 		relion({ ...testConfig })
 	})
 
 	it('should print latest release changelog', () => {
-		relion({ ...testConfig,
-			changelog: {
-				commitRange: 'latest-release',
-			},
-		})
+		relion({ ...testConfig, changelog: { commitRange: 'latest-release' } })
 	})
 
 	it('should print changelog for last 5 commits', () => {
-		relion({ ...testConfig,
-			changelog: {
-				commitRange: 'HEAD~5..',
-			},
-		})
-	})
-
-	it.for(['v0.7.0', 'v0.8.0'])('should print changelog for release $0', (releaseTag) => {
-		const result = relion({ ...testConfig,
-			changelog: {
-				commitRange: { releaseTag },
-			},
-		})
-		expect(result.generatedChangelog).toMatchSnapshot()
+		relion({ ...testConfig, changelog: { commitRange: 'HEAD~5..' } })
 	})
 })
