1b540ecb491f4205ac22592967952e5a81c54342
kh4f <kh4f.dev@gmail.com>
2025-09-25 11:37:00 +0300

    refactor(cli.test): separate manual CLI inspection tests

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,19 +1,17 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeAll } from 'vitest'
 import { runCli } from '@/cli'
+import type { UserConfig } from '@/types'
+import { logSpy } from './setup'
 
 describe('runCli', () => {
-	it('should output help message when `--help` is passed', async () => {
-		const exitSpy = vi.spyOn(process, 'exit').mockImplementation(() => undefined as never)
-		await runCli('--help', {})
-		expect(exitSpy).toHaveBeenCalledWith(0)
+	it('should update config options according to CLI flags', async () => {
+		expect((await runCli('--bump --changelog --commit --tag --dry', {}))?.inputConfig).toMatchObject({
+			bump: true, changelog: true, commit: true, tag: true, dryRun: true,
+		})
 	})
 
-	it('should simulate full release workflow', async () => {
-		await runCli('--bump --changelog --commit --tag --dry', {})
-	})
-
-	it(`should output latest release changelog using 'github' profile`, async () => {
-		await runCli('--changelog --profile github --latest', {
+	it(`should apply 'github' profile and 'latest-release' commit range`, async () => {
+		const config: UserConfig = {
 			_github: {
 				context: { commitHyperlink: false },
 				changelog: {
@@ -21,7 +19,19 @@ describe('runCli', () => {
 					partials: { header: '' },
 				},
 			},
-		})
+		}
+		const inputConfig = (await runCli('--changelog --profile github --latest', config))?.inputConfig
+		expect(inputConfig).toMatchObject({ ...config, profile: 'github', changelog: { commitRange: 'latest-release' } })
+	})
+})
+
+describe.runIf(process.env.VITEST_VSCODE)('manual runCli inspection', () => {
+	beforeAll(() => logSpy.mockRestore())
+
+	it('should output help message when `--help` is passed', async () => {
+		const exitSpy = vi.spyOn(process, 'exit').mockImplementation(() => undefined as never)
+		await runCli('--help', {})
+		expect(exitSpy).toHaveBeenCalledWith(0)
 	})
 
 	it('should load config from default path (relion.config.ts)', async () => {
