f2646f39eab8fe5f9f780a0494d7de646fb22dce
kh4f <kh4f.dev@gmail.com>
2025-09-28 10:19:52 +0300

    test(bump): add test for bumping version in `manifest.json`

--- tests/relion.test.ts
+++ tests/relion.test.ts
@@ -50,6 +50,21 @@ describe('source version resolution', () => {
 })
 
 describe('version bumping', () => {
+	it('should bump version in manifest.json', () => {
+		const origManifestContent = readFileSync('tests/fixtures/manifest.json', 'utf8')
+		relion({
+			dryRun: false,
+			context: { newVersion: '1.2.3' },
+			bump: ['!versionSourceFile', {
+				file: 'tests/fixtures/manifest.json',
+				pattern: /(version": )".*"/,
+				replacement: `$1"{{newVersion}}"`,
+			}],
+		})
+		expect(readFileSync('tests/fixtures/manifest.json', 'utf8')).toMatchSnapshot()
+		writeFileSync('tests/fixtures/manifest.json', origManifestContent, 'utf8')
+	})
+
 	it(`should append new version entry to 'versions.json'`, () => {
 		const origVersionsContent = readFileSync('tests/fixtures/versions.json', 'utf8')
 		relion({
