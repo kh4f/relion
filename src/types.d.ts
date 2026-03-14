2268401ea2d3175b894f62a881d0b5a7c9b645cc
kh4f <kh4f.dev@gmail.com>
2026-03-14 16:26:48 +0300

    docs(types): enhance JSDoc comments for `Cfg` interface properties

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,35 +1,30 @@
 export interface Cfg {
-	/** Manifest file path
-	 * @default 'package.json'
-	*/
+	/** Manifest file path (used to detect the package name and repo URL)
+	 * @default Autodetects 'package.json' or uses git info
+	 */
 	manifest?: string
 
-	/**
-	 * Files to bump version in
-	 * @default ['<manifest>']
+	/** Files to bump version in (manifest is always included if exists)
+	 * @default [<manifest>]
 	 */
 	bump?: string[]
 
-	/**
-	 * Explicitly set the version for the upcoming release
-	 * @default Determined automatically based on conventional commit messages
+	/** New version for the release
+	 * @default Auto-determined from conventional commits
 	 */
 	newVersion?: string
 
-	/**
-	 * Release commit message template
+	/** Release commit message template
 	 * @default 'chore(release): {{tag}}'
 	 */
 	commitMessage?: string
 
-	/**
-	 * Release tag prefix
+	/** Release tag prefix
 	 * @default 'v'
 	 */
 	tagPrefix?: string
 
-	/**
-	 * Run in dry mode without making any changes
+	/** Dry run mode
 	 * @default false
 	 */
 	dryRun?: boolean
